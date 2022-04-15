c11cde930e4f083849b024352dbdca600c8e744f
Maxaxik <17127859+maxaxik@users.noreply.github.com>
2022-04-15 19:42:08 +0200

    added reporting of more properties from API, improved consistency of quote marks, improved commenting

--- server.js
+++ server.js
@@ -8,37 +8,44 @@ const port = process.env.PORT || 3000; //1025 - 65535
 const { getSpotifyToken, getSongs, getSongFeatures } = require('./spotifyConnect')
 
 const server = http.createServer( async (req, res) => {
+	// a request comes in, log current time and req.url
 	let time = new Date().toISOString();
 	console.log(`New request at [${time}]: ${req.url}`);
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/plain');
 	
+	// pull out the query from the q= param
 	let url = new URL(`a://${req.url}`); // /?q=stuff
 	query = url.searchParams.get('q');
+
 	if (!query) {
-		// we don't like the input
+		// q param missing, send 400
 		res.statusCode = 400;
-		console.log(`Request was missing q parameter, rejected`);
+		console.log('Request was missing q parameter, rejected');
 		res.end('Error: Missing q parameter');
 		return;
 	}
 	try {
 		let songInfo = await fetchSongInfo(query);
+		// if songInfo is empty, we received no results, send 404
 		if (!songInfo) {
 			res.statusCode = 404;
-			console.log(`Search returned no results`);
-			res.end(`Search returned no results`);
+			console.log('Search returned no results');
+			res.end('Search returned no results');
 			return;
 		}
+		// else, format the results nice and send them, send 200
+		res.statusCode = 200;
 		responseMsg = formatSongInfo(songInfo);
-		console.log(`Request successful`);
+		console.log('Request successful');
 		res.end(responseMsg);
 		return;
 	} catch (err) {
+		// catch any error we got during the procees, log it and send 500
 		res.statusCode = 500;
-		console.log(`Request failed`);
+		console.log('Request failed');
 		console.log(err);
-		res.end(`Request failed`);
+		res.end('Request failed');
 		return;
 	}
 });
@@ -49,35 +56,47 @@ server.listen(port, () => {
 
 
 function formatSongInfo(songInfo) {
-	let returnString = "";
+	// is formatting strings always this messy
+	let returnString = '';
+	// properties returned from the API that we don't want on the second line
+	let dontWant = ['artist', 'name', 'type', 'id', 'uri', 'track_href', 'analysis_url'];
+	
 	songInfo.forEach(song => {
 		returnString += `${song.artist} - ${song.name}\n`
-		returnString += `bpm:${song.bpm},time_signature:${song.time_signature},danceability:${song.danceability}\n`
+		let featuresList = [];
+		for (let songFeature of Object.keys(song)) {
+			// skip things from dontWant
+			if (dontWant.includes(songFeature)) continue;
+			
+			// push other things into featuresList
+			featuresList.push(`${songFeature}:${song[songFeature]}`);
+		}
+		returnString += featuresList.join(',')+'\n';
 	});
 	return returnString;
 }
 
 async function fetchSongInfo(query) {
-	// get a Bearer token from Spotify, reuse the one we have if we do
+	// get a Bearer token from Spotify, reuse the one we have if it's still valid
 	token = await getSpotifyToken(client_id, client_secret);
 
-	// get rid of stuff in parentheses and brackets, as that seems to mess up the search
+	// get rid of stuff in parentheses and brackets from the query, as that seems to mess up the search
 	query = query.replace(/(\[(.*?)\])|(\((.*?)\))/g, '');
 
-	// query Spotify with the string to find the appropriate song
+	// query Spotify with the string to find the song
 	let songs = await getSongs(query, token);
 	
 	// return if we didn't get any results
 	if (songs.tracks.total == 0) return; 
 
-	// Format the search results to keep track of which song is which
+	// format the search results to keep track of which song is which
 	// https://developer.spotify.com/documentation/web-api/reference/#/operations/search
 	// Map( id:{name:"", bpm:42},id:{name:"", bpm:42},... )
 	let songMap = new Map();
 	songs.tracks.items
 		.forEach(song => songMap.set(song.id, {name: song.name, artist: song.artists[0].name}));
 	
-	// Format the id list for Audio Features search
+	// format the id list for Audio Features search
 	// https://developer.spotify.com/documentation/web-api/reference/#/operations/get-several-audio-features
 	// "id,id,id,id,id"
 	let IDstring = songs.tracks.items
@@ -85,12 +104,12 @@ async function fetchSongInfo(query) {
 		.join(',');
 
 	let songFeatures = await getSongFeatures(IDstring, token);
-	// Combine info from Audio Features search into the song map
+
+	// combine info from Audio Features search into the song Map
 	songFeatures.audio_features.forEach(song => {
-		const existingSong = songMap.get(song.id);
-		existingSong.bpm = song.tempo;
-		existingSong.time_signature = song.time_signature;
-		existingSong.danceability = song.danceability;
+		let existingSong = songMap.get(song.id);
+		existingSong = {...existingSong, ...song};
+		songMap.set(song.id, existingSong);
 	});
 
 	return songMap;
--- spotifyConnect.js
+++ spotifyConnect.js
@@ -6,11 +6,11 @@ let SpotifyToken;
 
 module.exports = {
 	async getSpotifyToken(client_id, client_secret) {
-		// If we have a valid token return it
+		// if we have a valid token return it
 		if (TokenExpires > Date.now()) {
 			return SpotifyToken;
 		}
-		// Else ask Spotify for a token
+		// else ask Spotify for a token
 		const params = new url.URLSearchParams({ 'grant_type': 'client_credentials' });
 		const res = await axios.post('https://accounts.spotify.com/api/token', 
 			params.toString(),
@@ -21,13 +21,14 @@ module.exports = {
 				}
 			}
 		)
-
+		// keep track of how long the token is valid for
 		TokenExpires = res.data.expires_in * 1000 + Date.now();
 		SpotifyToken = res.data.access_token;
 		return SpotifyToken;
 	},
 
 	async getSongs(query, token) {
+		// query spotify API for 5 tracks with our query
 		const res = await axios.get('https://api.spotify.com/v1/search', {
 			params: {
 				type: 'track',
@@ -39,20 +40,21 @@ module.exports = {
 				'Content-Type': 'application/json'
 			}
 		});
-
+		
 		return res.data;
 	},
-
+	
 	async getSongFeatures(query, token) {
+		// query spotify API for all our tracks at once
 		const res = await axios.get('https://api.spotify.com/v1/audio-features', {
-				params: {
-					ids: query
-				},
-				headers: {
-					'Authorization': `Bearer ${token}`,
-					'Content-Type': 'application/json'
-				}
-			});
+			params: {
+				ids: query
+			},
+			headers: {
+				'Authorization': `Bearer ${token}`,
+				'Content-Type': 'application/json'
+			}
+		});
 
 		return res.data;
 	}
